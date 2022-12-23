48907ba4755f3afaf7a17c67f983f654b62af3b8
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-23 20:40:26 +0100

    test: three years old

--- katas/8_kyu/parse_nice_int_from_char/src/lib.rs
+++ katas/8_kyu/parse_nice_int_from_char/src/lib.rs
@@ -11,4 +11,9 @@ mod tests {
     fn it_returns_2_for_two_years_old() {
         assert_eq!(get_age("2 years old"), 2);
     }
+
+    #[test]
+    fn it_returns_3_for_three_years_old() {
+        assert_eq!(get_age("3 years old"), 3);
+    }
 }
