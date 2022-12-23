f5578df3b2d0cdf57e650c8607214c71c81e8fe0
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-23 23:50:21 +0100

    test: change lower to upper

--- katas/8_kyu/to_alternate_case/src/lib.rs
+++ katas/8_kyu/to_alternate_case/src/lib.rs
@@ -10,4 +10,8 @@ mod tests {
     fn it_works() {
         assert!(true);
     }
+    #[test]
+    fn it_should_change_casing_from_lower_to_upper() {
+        assert_eq!(to_alternating_case("a"), "A");
+    }
 }
