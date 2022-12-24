bda1475b48cfa2cc2b74fb30f8593343a5c04f7e
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-24 16:39:43 +0100

    feat: handle 1 and 2 elements

--- katas/8_kyu/sum_of_differences_in_array/src/lib.rs
+++ katas/8_kyu/sum_of_differences_in_array/src/lib.rs
@@ -1,7 +1,11 @@
 #![allow(dead_code, unused_variables, unused_imports)]
 
 fn sum_of_differences(arr: &[i8]) -> Option<i8> {
-    None
+    if arr.len() == 1 {
+        None
+    } else {
+        Some(1)
+    }
 }
 
 #[cfg(test)]
@@ -16,5 +20,10 @@ mod tests {
     #[test]
     fn it_returns_none_if_only_one_element() {
         assert_eq!(sum_of_differences(&[1]), None);
+    }
+
+    #[test]
+    fn it_returns_one_for_2_and_1() {
+        assert_eq!(sum_of_differences(&[2, 1]), Some(1));
     }
 }
