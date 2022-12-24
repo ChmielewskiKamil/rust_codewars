5a843b3c3b0f4d8c3841027bf130ad5361771fd7
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-24 21:54:30 +0100

    feat: handle different combinations of n and x

--- katas/8_kyu/count_by_x/src/lib.rs
+++ katas/8_kyu/count_by_x/src/lib.rs
@@ -1,5 +1,13 @@
+use std::vec;
+
+/// `x` is the number to be multiplied
+/// `n` is how many times `x` will be multiplied
 fn count_by(x: u32, n: u32) -> Vec<u32> {
-    vec![1]
+    let mut v: Vec<u32> = vec![];
+    for i in 1..n + 1 {
+        v.push(x * i);
+    }
+    v
 }
 
 #[cfg(test)]
@@ -14,5 +22,15 @@ mod tests {
     #[test]
     fn it_returns_1_for_1_1() {
         assert_eq!(count_by(1, 1), vec![1]);
+    }
+
+    #[test]
+    fn it_returns_2_elements_for_1_2() {
+        assert_eq!(count_by(1, 2), vec![1, 2]);
+    }
+
+    #[test]
+    fn it_returns_10_elements_for_2_10() {
+        assert_eq!(count_by(2, 10), vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20]);
     }
 }
