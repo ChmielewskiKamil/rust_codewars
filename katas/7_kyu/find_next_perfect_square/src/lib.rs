a0faef41a26a2c3661f8bb7997735e6a30dfb02c
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-29 19:58:14 +0100

    refactor: make code more idiomatic

--- katas/7_kyu/find_next_perfect_square/src/lib.rs
+++ katas/7_kyu/find_next_perfect_square/src/lib.rs
@@ -1,7 +1,7 @@
 fn find_next_square(sq: u64) -> Option<u64> {
     let square_root = (sq as f64).sqrt();
     if square_root.floor() == square_root {
-        return Some(((square_root as u64) + 1).pow(2));
+        Some(((square_root as u64) + 1).pow(2))
     } else {
         None
     }
