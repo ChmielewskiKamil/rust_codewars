d5698e752bfa5bf9bd2459a720e49987ffd09764
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-30 21:21:56 +0100

    refactor: add array of roman literals

--- katas/6_kyu/roman_numerals_encoder/src/lib.rs
+++ katas/6_kyu/roman_numerals_encoder/src/lib.rs
@@ -7,6 +7,22 @@ fn num_as_roman(mut num: i32) -> String {
             num
         );
     }
+    /// Array of tuples (roman, value)
+    let numerals = [
+        ("M", 1000),
+        ("CM", 900),
+        ("D", 500),
+        ("CD", 400),
+        ("C", 100),
+        ("XC", 90),
+        ("L", 50),
+        ("XL", 40),
+        ("X", 10),
+        ("IX", 9),
+        ("V", 5),
+        ("IV", 4),
+        ("I", 1),
+    ];
 
     while num > 0 {
         match num {
