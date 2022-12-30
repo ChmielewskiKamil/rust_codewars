153ae0812179825d8fefb65b69efabc1b7b30f9e
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-30 20:49:08 +0100

    feat: handle numbers <= 100

--- katas/6_kyu/roman_numerals_encoder/src/lib.rs
+++ katas/6_kyu/roman_numerals_encoder/src/lib.rs
@@ -3,6 +3,10 @@ fn num_as_roman(mut num: i32) -> String {
     let mut roman_number = String::new();
     while num > 0 {
         match num {
+            n if n >= 100 => {
+                roman_number.push_str("C");
+                num -= 100;
+            }
             n if n >= 90 => {
                 roman_number.push_str("XC");
                 num -= 90;
