79ea9dacc9d8db6362415cf2bcb09ce6c0c22330
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-30 20:53:28 +0100

    test: handle numbers <= 900

--- katas/6_kyu/roman_numerals_encoder/src/lib.rs
+++ katas/6_kyu/roman_numerals_encoder/src/lib.rs
@@ -3,6 +3,10 @@ fn num_as_roman(mut num: i32) -> String {
     let mut roman_number = String::new();
     while num > 0 {
         match num {
+            n if n >= 900 => {
+                roman_number.push_str("CM");
+                num -= 900;
+            }
             n if n >= 500 => {
                 roman_number.push_str("D");
                 num -= 500;
